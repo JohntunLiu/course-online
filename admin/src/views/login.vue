407b89bc18363f4a4058c19f532bc82aebf6a739
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-04-11 18:34:45 +0800

    增加admin页面

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -50,7 +50,7 @@
                             <span class="lbl"> 记住我</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button"  class="width-35 pull-right btn btn-sm btn-primary">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">登录</span>
                           </button>
