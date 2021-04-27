0a5f71f1485c3c724844d71c87a8902fedd26b73
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-04-27 09:23:31 +0800

    1、增加工具类tool.js 校验类validator.js 2、增加大章保存 对输入框的校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -146,6 +146,13 @@ export default {
 
     save() {
       let _this = this;
+
+      if (!Validator.require(_this.chapter.name, '名称')
+          || !Validator.require(_this.chapter.courseId, "课程ID")
+          || !Validator.length(_this.chapter.courseId, "课程ID", 1,8)) {
+        return;
+      }
+
       Loading.show();
 
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',
