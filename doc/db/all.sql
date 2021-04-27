caf492d98a9c2ce02c844864fced180ed4608419
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-04-27 12:15:11 +0800

    1. 创建小章表结构生成表 2. 生成持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -57,8 +57,8 @@ create table `section` (
   `time` int comment '时长|单位秒',
   `charge` char(1) comment '收费|C 收费；F 免费',
   `sort` int comment '顺序',
-  `created_at` datetime(3) comment '创建时间',
-  `updated_at` datetime(3) comment '修改时间',
+  `created_at` datetime comment '创建时间',
+  `updated_at` datetime comment '修改时间',
   primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='小节';
 
