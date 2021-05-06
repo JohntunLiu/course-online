baa8e03677f9cf4e651abea4faf4164f50adf959
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-06 13:45:45 +0800

    1. 分类表生成和基本代码生成

---
+++ business/src/main/java/com/course/business/controller/admin/CategoryController.java
@@ -0,0 +1,74 @@
+package com.course.business.controller.admin;
+
+
+import com.course.server.dto.CategoryDto;
+import com.course.server.dto.PageDto;
+import com.course.server.dto.ResponseDto;
+import com.course.server.service.CategoryService;
+import com.course.server.util.ValidatorUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@ComponentScan({"com.course.server"})
+@RequestMapping("/admin/category")
+@Slf4j
+public class CategoryController {
+
+public static final String BUSINESS_NAME = "分类";
+
+
+@Resource
+private CategoryService categoryService;
+
+/**
+* 查询
+*
+*/
+@PostMapping("/list")
+public ResponseDto list(@RequestBody PageDto pageDto) {
+ResponseDto responseDto = new ResponseDto();
+
+categoryService.list(pageDto);
+responseDto.setContent(pageDto);
+return responseDto;
+}
+
+/**
+* 保存
+* 有id时更新 无id时插入
+*/
+@PostMapping("/save")
+public ResponseDto save(@RequestBody CategoryDto categoryDto) {
+
+//保存校验
+
+            ValidatorUtil.require(categoryDto.getParent(), "父id");
+            ValidatorUtil.require(categoryDto.getName(), "名称");
+            ValidatorUtil.length(categoryDto.getName(), "名称", 1, 50);
+
+ResponseDto responseDto = new ResponseDto();
+
+categoryService.save(categoryDto);
+responseDto.setContent(categoryDto);
+
+return responseDto;
+}
+
+/**
+* 删除
+*
+*/
+@PostMapping("/delete/{id}")
+public ResponseDto save(@PathVariable String   id) {
+ResponseDto responseDto = new ResponseDto();
+
+categoryService.delete(id);
+
+return responseDto;
+}
+
+}
