270bc864415b24d21bd9eff6d677e70d0f3c1c35
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-09 16:02:40 +0800

    1. 文件上传功能开发：去掉文件管理的新增, 修改, 删除功能

--- file/src/main/java/com/course/file/controller/admin/FileController.java
+++ file/src/main/java/com/course/file/controller/admin/FileController.java
@@ -1,14 +1,15 @@
 package com.course.file.controller.admin;
 
 
-import com.course.server.dto.FileDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.FileService;
-import com.course.server.util.ValidatorUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -18,59 +19,24 @@ import javax.annotation.Resource;
 @Slf4j
 public class FileController {
 
-public static final String BUSINESS_NAME = "文件";
+    public static final String BUSINESS_NAME = "文件";
 
 
-@Resource
-private FileService fileService;
+    @Resource
+    private FileService fileService;
 
-/**
-* 查询
-*
-*/
-@PostMapping("/list")
-public ResponseDto list(@RequestBody PageDto pageDto) {
-ResponseDto responseDto = new ResponseDto();
+    /**
+     * 查询
+     */
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto) {
+        ResponseDto responseDto = new ResponseDto();
 
-fileService.list(pageDto);
-responseDto.setContent(pageDto);
-return responseDto;
-}
+        fileService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
 
-/**
-* 保存
-* 有id时更新 无id时插入
-*/
-@PostMapping("/save")
-public ResponseDto save(@RequestBody FileDto fileDto) {
 
-//保存校验
-
-            ValidatorUtil.require(fileDto.getPath(), "相对路径");
-            ValidatorUtil.length(fileDto.getPath(), "相对路径", 1, 100);
-            ValidatorUtil.length(fileDto.getName(), "文件名", 1, 100);
-            ValidatorUtil.length(fileDto.getSuffix(), "后缀", 1, 10);
-            ValidatorUtil.length(fileDto.getKey(), "文件标识", 1, 32);
-
-ResponseDto responseDto = new ResponseDto();
-
-fileService.save(fileDto);
-responseDto.setContent(fileDto);
-
-return responseDto;
-}
-
-/**
-* 删除
-*
-*/
-@PostMapping("/delete/{id}")
-public ResponseDto save(@PathVariable String   id) {
-ResponseDto responseDto = new ResponseDto();
-
-fileService.delete(id);
-
-return responseDto;
-}
 
 }
