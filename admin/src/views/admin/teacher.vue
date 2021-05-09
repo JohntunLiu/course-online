f87e9b58f96188c21ccc6857606c2dcc68962329
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-09 13:25:38 +0800

    1. 文件上传功能开发：优化表单图片显示

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -90,7 +90,11 @@
                      <label class="col-sm-2 control-label">头像</label>
                      <div class="col-sm-10">
                        <input type="file" @change="uploadImage()" id="file-upload-input">
-                       <img v-bind:src="teacher.image" class="img-responsive">
+                       <div v-show="teacher.image" class="row">
+                         <div class="col-md-4">
+                           <img v-bind:src="teacher.image" class="img-responsive">
+                         </div>
+                       </div>
                      </div>
                    </div>
                    <div class="form-group">
