0559c7ebd2de798f2a445d53fa21a96c3c438934
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-09 16:27:13 +0800

    1. 文件上传功能开发：文件按用途分类保存

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -95,9 +95,10 @@
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
                   <file v-bind:input-id="'image-upload'"
-                            v-bind:text="'上传头像'"
-                            v-bind:suffixs="['jpg', 'jpeg', 'png']"
-                            v-bind:after-upload="afterUpload"></file>
+                        v-bind:text="'上传头像'"
+                        v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                        v-bind:use="FILE_USE.TEACHER.key"
+                        v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -151,6 +152,7 @@ export default {
     return {
       teacher: {},
       teachers: [],
+      FILE_USE: FILE_USE,
     }
   },
 
--- file/src/main/java/com/course/file/controller/admin/UploadController.java
+++ file/src/main/java/com/course/file/controller/admin/UploadController.java
@@ -2,6 +2,7 @@ package com.course.file.controller.admin;
 
 import com.course.server.dto.FileDto;
 import com.course.server.dto.ResponseDto;
+import com.course.server.enums.FileUseEnum;
 import com.course.server.service.FileService;
 import com.course.server.util.UuidUtil;
 import org.slf4j.Logger;
@@ -39,16 +40,24 @@ public class UploadController {
     private FileService fileService;
 
     @RequestMapping("/upload")
-    public ResponseDto upload(@RequestParam MultipartFile file) throws Exception {
-        LOG.info("上传文件开始:{}", file);
+    public ResponseDto upload(@RequestParam MultipartFile file, String use) throws Exception {
+        LOG.info("上传文件开始" );
         LOG.info(file.getOriginalFilename());
         LOG.info(String.valueOf(file.getSize()));
 
+        FileUseEnum useEnum = FileUseEnum.getByCode(use);
         String key = UuidUtil.getShortUuid();
         String fileName = file.getOriginalFilename();
         String suffix = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
-        String path = "teacher/" + key + "." + suffix;
 
+        //如果文件不存在就创建
+        String dir = useEnum.name().toLowerCase();
+        File fullDir = new File(FILE_PATH + dir);
+        if (!fullDir.exists()) {
+            fullDir.mkdirs();
+        }
+
+        String path = dir+ File.separator + key + "." + suffix;
         String fullPath = FILE_PATH + path;
         File dest = new File(fullPath);
         file.transferTo(dest);
@@ -60,7 +69,7 @@ public class UploadController {
         fileDto.setName(fileName);
         fileDto.setSize(Math.toIntExact(file.getSize()));
         fileDto.setSuffix(suffix);
-        fileDto.setUse("");
+        fileDto.setUse(use);
         fileService.save(fileDto);
 
         ResponseDto responseDto = new ResponseDto();
