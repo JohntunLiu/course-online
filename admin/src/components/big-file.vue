27c6d05f082463db56dd4aa95b59104a8e704954
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-11 09:02:08 +0800

    1. 文件断点续传与极速秒传: 修改为基于OSS接口的文件上传

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -80,9 +80,9 @@ export default {
       }
 
       // 文件分片
-      let shardSize = 20 * 1024 * 1024;    //以10MB为一个分片
+      // let shardSize = 20 * 1024 * 1024;    //以10MB为一个分片
       let shardIndex = 1;		//分片索引，1表示第1个分片
-      // let shardSize = 50 * 1024;    //以50KB为一个分片
+      let shardSize = 50 * 1024;    //以50KB为一个分片
       let size = file.size;
       let shardTotal = Math.ceil(size / shardSize); //总片数
 
@@ -168,7 +168,7 @@ export default {
         let base64 = e.target.result;
         // console.log("base64", base64);
         param.shard = base64;
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', param).then((response) => {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/oss-append', param).then((response) => {
           // Loading.hide();
           let resp = response.data;
           console.log("上传文件成功：", resp);
--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -94,11 +94,11 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                  <big-file v-bind:input-id="'image-upload'"
+                  <file v-bind:input-id="'image-upload'"
                         v-bind:text="'上传头像'"
                         v-bind:suffixs="['jpg', 'jpeg', 'png']"
                         v-bind:use="FILE_USE.TEACHER.key"
-                        v-bind:after-upload="afterUpload"></big-file>
+                        v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -141,12 +141,14 @@
 
 import Pagination from "../../components/pagination"
 import BigFile from "../../components/big-file"
+import File from "../../components/file"
 
 export default {
   name: 'business-teacher',
   components: {
     Pagination,
     BigFile,
+    File,
   },
   data: function () {
     return {
