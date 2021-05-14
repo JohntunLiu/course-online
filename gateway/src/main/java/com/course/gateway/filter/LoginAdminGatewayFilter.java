788ed8849549864020b77ee843cb8f686d2bbeb2
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-14 19:55:48 +0800

    1. 用户管理与登录: 在gateway中增加登录拦截器

---
+++ gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
@@ -0,0 +1,39 @@
+package com.course.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+
+@Component
+@Slf4j
+public class LoginAdminGatewayFilter implements GatewayFilter, Ordered {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LoginAdminGatewayFilter.class);
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getHeaders().getFirst("token");
+        log.info("登录拦截开始: {}", token);
+
+        return chain.filter(exchange);
+    }
+
+
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
