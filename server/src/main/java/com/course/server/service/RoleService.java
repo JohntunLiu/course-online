fa93c870b8e50e0077079310750320a12c897247
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-15 16:48:35 +0800

    1. 通用权限管理: 点击资源树模态框[保存]按钮时, 保存角色关联表

--- server/src/main/java/com/course/server/service/RoleService.java
+++ server/src/main/java/com/course/server/service/RoleService.java
@@ -2,14 +2,18 @@ package com.course.server.service;
 
 import com.course.server.domain.Role;
 import com.course.server.domain.RoleExample;
-import com.course.server.dto.RoleDto;
+import com.course.server.domain.RoleResource;
+import com.course.server.domain.RoleResourceExample;
 import com.course.server.dto.PageDto;
+import com.course.server.dto.RoleDto;
 import com.course.server.mapper.RoleMapper;
+import com.course.server.mapper.RoleResourceMapper;
 import com.course.server.util.CopyUtil;
 import com.course.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -22,11 +26,14 @@ public class RoleService {
     @Resource
     private RoleMapper roleMapper;
 
+    @Resource
+    private RoleResourceMapper roleResourceMapper;
+
     /**
-    * 列表查询
-    */
+     * 列表查询
+     */
     public void list(PageDto pageDto) {
-        PageHelper.startPage(pageDto.getPage(),pageDto.getSize());
+        PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         RoleExample roleExample = new RoleExample();
 
 
@@ -42,31 +49,56 @@ public class RoleService {
 
         Role role = CopyUtil.copy(roleDto, Role.class);
         if (StringUtils.isEmpty(roleDto.getId())) {
-        this.insert(role);
+            this.insert(role);
         } else {
-        this.update(role);
+            this.update(role);
         }
 
     }
+
     /**
-    * 新增
-    */
+     * 新增
+     */
     private void insert(Role role) {
         role.setId(UuidUtil.getShortUuid());
         roleMapper.insert(role);
     }
 
     /**
-    * 更新
-    */
+     * 更新
+     */
     private void update(Role role) {
         roleMapper.updateByPrimaryKey(role);
     }
 
     /**
-    * 删除
-    */
+     * 删除
+     */
     public void delete(String id) {
-    roleMapper.deleteByPrimaryKey(id);
+        roleMapper.deleteByPrimaryKey(id);
     }
+
+    /**
+     * 按角色保存资源
+     */
+    @Transactional
+    public void saveResource(RoleDto roleDto) {
+        String roleId = roleDto.getId();
+        List<String> resourceIds = roleDto.getResourceIds();
+        // 清空库中所有的当前角色下的记录
+        RoleResourceExample example = new RoleResourceExample();
+        example.createCriteria().andRoleIdEqualTo(roleId);
+        roleResourceMapper.deleteByExample(example);
+
+        // 保存角色资源
+        for (int i = 0; i < resourceIds.size(); i++) {
+            RoleResource roleResource = new RoleResource();
+            roleResource.setId(UuidUtil.getShortUuid());
+            roleResource.setRoleId(roleId);
+            roleResource.setResourceId(resourceIds.get(i));
+            roleResourceMapper.insert(roleResource);
+        }
+    }
+
+
 }
--- system/src/main/java/com/course/system/controller/admin/RoleController.java
+++ system/src/main/java/com/course/system/controller/admin/RoleController.java
@@ -18,58 +18,69 @@ import javax.annotation.Resource;
 @Slf4j
 public class RoleController {
 
-public static final String BUSINESS_NAME = "角色";
+    public static final String BUSINESS_NAME = "角色";
 
 
-@Resource
-private RoleService roleService;
+    @Resource
+    private RoleService roleService;
 
-/**
-* 查询
-*
-*/
-@PostMapping("/list")
-public ResponseDto list(@RequestBody PageDto pageDto) {
-ResponseDto responseDto = new ResponseDto();
+    /**
+     * 查询
+     */
+    @PostMapping("/list")
+    public ResponseDto list(@RequestBody PageDto pageDto) {
+        ResponseDto responseDto = new ResponseDto();
 
-roleService.list(pageDto);
-responseDto.setContent(pageDto);
-return responseDto;
-}
+        roleService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
 
-/**
-* 保存
-* 有id时更新 无id时插入
-*/
-@PostMapping("/save")
-public ResponseDto save(@RequestBody RoleDto roleDto) {
+    /**
+     * 保存
+     * 有id时更新 无id时插入
+     */
+    @PostMapping("/save")
+    public ResponseDto save(@RequestBody RoleDto roleDto) {
 
 //保存校验
 
-            ValidatorUtil.require(roleDto.getName(), "角色");
-            ValidatorUtil.length(roleDto.getName(), "角色", 1, 50);
-            ValidatorUtil.require(roleDto.getDesc(), "描述");
-            ValidatorUtil.length(roleDto.getDesc(), "描述", 1, 100);
+        ValidatorUtil.require(roleDto.getName(), "角色");
+        ValidatorUtil.length(roleDto.getName(), "角色", 1, 50);
+        ValidatorUtil.require(roleDto.getDesc(), "描述");
+        ValidatorUtil.length(roleDto.getDesc(), "描述", 1, 100);
 
-ResponseDto responseDto = new ResponseDto();
+        ResponseDto responseDto = new ResponseDto();
 
-roleService.save(roleDto);
-responseDto.setContent(roleDto);
+        roleService.save(roleDto);
+        responseDto.setContent(roleDto);
 
-return responseDto;
-}
+        return responseDto;
+    }
 
-/**
-* 删除
-*
-*/
-@PostMapping("/delete/{id}")
-public ResponseDto save(@PathVariable String   id) {
-ResponseDto responseDto = new ResponseDto();
+    /**
+     * 删除
+     */
+    @PostMapping("/delete/{id}")
+    public ResponseDto save(@PathVariable String id) {
+        ResponseDto responseDto = new ResponseDto();
 
-roleService.delete(id);
+        roleService.delete(id);
 
-return responseDto;
-}
+        return responseDto;
+    }
+
+    /**
+     * 保存资源
+     * @param roleDto
+     */
+    @PostMapping("/save-resource")
+    public ResponseDto saveResource(@RequestBody RoleDto roleDto) {
+        log.info("保存角色资源关联开始");
+        ResponseDto<RoleDto> responseDto = new ResponseDto<>();
+        roleService.saveResource(roleDto);
+        responseDto.setContent(roleDto);
+        return responseDto;
+    }
 
 }
