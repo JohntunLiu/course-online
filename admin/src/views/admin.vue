5808562090d94409ed5bdcbb6194ffef01b48ace
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-16 17:27:48 +0800

    1. 通过权限管理: 前端界面权限拦截, 完成用户管理

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -16,7 +16,7 @@
           <a href="index.html" class="navbar-brand">
             <small>
               <i class="fa fa-leaf"></i>
-              管理控制台
+              在线视频课程
             </small>
           </a>
         </div>
@@ -83,7 +83,7 @@
                           <span class="pull-right">90%</span>
                         </div>
 
-                        <div class="progress progress-mini progress-striped ">
+                        <div class="progress progress-mini progress-striped active">
                           <div style="width:90%" class="progress-bar progress-bar-success"></div>
                         </div>
                       </a>
@@ -282,7 +282,6 @@
               <a data-toggle="dropdown" href="#" class="dropdown-toggle">
                 <img class="nav-user-photo" src="../../public/ace/assets/images/avatars/user.jpg" alt="Jason's Photo" />
 
-
                 <i class="ace-icon fa fa-caret-down"></i>
               </a>
 
@@ -304,7 +303,7 @@
                 <li class="divider"></li>
 
                 <li>
-                  <a @click="logout()" href="#">
+                  <a v-on:click="logout()" href="#">
                     <i class="ace-icon fa fa-power-off"></i>
                     退出登录
                   </a>
@@ -317,10 +316,7 @@
     </div>
 
     <div class="main-container ace-save-state" id="main-container">
-
-
       <div id="sidebar" class="sidebar                  responsive                    ace-save-state">
-
 
         <div class="sidebar-shortcuts" id="sidebar-shortcuts">
           <div class="sidebar-shortcuts-large" id="sidebar-shortcuts-large">
@@ -356,15 +352,13 @@
           <li class="" id="welcome-sidebar">
             <router-link to="/welcome">
               <i class="menu-icon fa fa-tachometer"></i>
-              <span class="menu-text"> 欢迎：{{loginUser.name}}</span>
+              <span class="menu-text"> 欢迎：{{loginUser.name}} </span>
             </router-link>
 
             <b class="arrow"></b>
           </li>
 
-
-
-          <li class="">
+          <li v-show="hasResource('01')" class="">
             <a href="#" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
               <span class="menu-text"> 系统管理 </span>
@@ -375,7 +369,7 @@
             <b class="arrow"></b>
 
             <ul class="submenu">
-              <li class="" id="system-user-sidebar">
+              <li v-show="hasResource('0101')" class="" id="system-user-sidebar">
                 <router-link to="/system/user">
                   <i class="menu-icon fa fa-caret-right"></i>
                   用户管理
@@ -384,7 +378,7 @@
                 <b class="arrow"></b>
               </li>
 
-              <li class=""  id="system-resource-sidebar">
+              <li v-show="hasResource('0102')" class="" id="system-resource-sidebar">
                 <router-link to="/system/resource">
                   <i class="menu-icon fa fa-caret-right"></i>
                   资源管理
@@ -393,7 +387,7 @@
                 <b class="arrow"></b>
               </li>
 
-              <li class=""  id="system-role-sidebar">
+              <li v-show="hasResource('0103')" class="" id="system-role-sidebar">
                 <router-link to="/system/role">
                   <i class="menu-icon fa fa-caret-right"></i>
                   角色管理
@@ -404,32 +398,7 @@
             </ul>
           </li>
 
-          <li class="">
-            <a href="#" class="dropdown-toggle">
-              <i class="menu-icon fa fa-list"></i>
-              <span class="menu-text"> 文件管理 </span>
-
-              <b class="arrow fa fa-angle-down"></b>
-            </a>
-
-            <b class="arrow"></b>
-
-            <ul class="submenu">
-              <li class="" id="file-file-sidebar">
-                <router-link to="/file/file">
-                  <i class="menu-icon fa fa-caret-right"></i>
-                  文件管理
-                </router-link>
-
-                <b class="arrow"></b>
-              </li>
-
-            </ul>
-          </li>
-
-
-
-          <li class="">
+          <li v-show="hasResource('02')" class="">
             <a href="#" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
               <span class="menu-text"> 业务管理 </span>
@@ -440,29 +409,69 @@
             <b class="arrow"></b>
 
             <ul class="submenu">
-              <li class="" id="business-category-sidebar">
+              <li v-show="hasResource('0201')" class="" id="business-category-sidebar">
                 <router-link to="/business/category">
                   <i class="menu-icon fa fa-caret-right"></i>
                   分类管理
                 </router-link>
+
                 <b class="arrow"></b>
               </li>
-              <li class="" id="business-course-sidebar">
+              <li v-show="hasResource('0202')" class="" id="business-course-sidebar">
                 <router-link to="/business/course">
                   <i class="menu-icon fa fa-caret-right"></i>
                   课程管理
                 </router-link>
+
                 <b class="arrow"></b>
               </li>
-              <li class="" id="business-teacher-sidebar">
+              <li v-show="hasResource('0203')" class="" id="business-teacher-sidebar">
                 <router-link to="/business/teacher">
                   <i class="menu-icon fa fa-caret-right"></i>
                   讲师管理
                 </router-link>
+
+                <b class="arrow"></b>
+              </li>
+              <li v-show="hasResource('0204')" class="" id="business-member-sidebar">
+                <router-link to="/business/member">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  会员管理
+                </router-link>
+
+                <b class="arrow"></b>
+              </li>
+              <li v-show="hasResource('0205')" class="" id="business-sms-sidebar">
+                <router-link to="/business/sms">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  短信管理
+                </router-link>
+
                 <b class="arrow"></b>
               </li>
 
+            </ul>
+          </li>
 
+          <li v-show="hasResource('03')" class="">
+            <a href="#" class="dropdown-toggle">
+              <i class="menu-icon fa fa-list"></i>
+              <span class="menu-text"> 文件管理 </span>
+
+              <b class="arrow fa fa-angle-down"></b>
+            </a>
+
+            <b class="arrow"></b>
+
+            <ul class="submenu">
+              <li v-show="hasResource('0301')" class="" id="file-file-sidebar">
+                <router-link to="/file/file">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  文件管理
+                </router-link>
+
+                <b class="arrow"></b>
+              </li>
 
             </ul>
           </li>
@@ -480,7 +489,7 @@
             <div class="row">
               <div class="col-xs-12">
                 <!-- PAGE CONTENT BEGINS -->
-                <router-view></router-view>
+                <router-view/>
                 <!-- PAGE CONTENT ENDS -->
               </div><!-- /.col -->
             </div><!-- /.row -->
@@ -492,8 +501,8 @@
         <div class="footer-inner">
           <div class="footer-content">
 						<span class="bigger-120">
-							<span class="blue bolder">Ace</span>
-							Application &copy; 2013-2014
+							<span class="blue bolder">Tang</span>
+							在线教学视频 &copy; 2099-2099
 						</span>
 
             &nbsp; &nbsp;
@@ -518,95 +527,114 @@
         <i class="ace-icon fa fa-angle-double-up icon-only bigger-110"></i>
       </a>
     </div><!-- /.main-container -->
-
   </div>
 </template>
 
 <script>
-
-
-
-export default {
-  name: 'admin',
-  data: function () {
-    return {
-      loginUser: {},
-      imageCodeToken: ""
-    }
-  },
-  mounted() {
-    let _this = this;
-
-    $('body').removeClass('login-layout light-login');
-    $('body').attr('class', 'no-skin');
-
-    _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
-
-    $.getScript('/ace/assets/js/ace.min.js');
-
-    _this.loginUser = Tool.getLoginUser();
-
-
-  },
-  watch: {
-    $route: {
-      handler:function(val, oldVal){
-        // sidebar激活样式方法二
-        console.log("---->页面跳转：", val, oldVal);
-        let _this = this;
-
-        // if (!_this.hasResourceRouter(val.name)) {
-        //   _this.$router.push("/login");
-        //   return;
-        // }
-
-        _this.$nextTick(function(){  //页面加载完成后执行
-          _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
-        })
+  export default {
+    name: "admin",
+    data: function() {
+      return {
+        loginUser: {},
       }
-    }
-  },
-  methods: {
-
-    login () {
-      this.$router.push("/admin")
     },
-
-    logout() {
+    mounted: function() {
       let _this = this;
-      _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/user/logout/' + _this.loginUser.token).then((response)=>{
-        Loading.hide();
-        let resp = response.data;
-        if (resp.success) {
-          Tool.setLoginUser(null);
-          _this.$router.push("/login");
-        } else {
-          Toast.warning(resp.message);
-        }
-      });
+      $("body").removeClass("login-layout light-login");
+      $("body").attr("class", "no-skin");
+      // console.log("admin");
+      // sidebar激活样式方法二
+      _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+
+      $.getScript('/ace/assets/js/ace.min.js');
+
+      _this.loginUser = Tool.getLoginUser();
+
+      // if (!_this.hasResourceRouter(_this.$route.name)) {
+      //   _this.$router.push("/login");
+      // }
     },
+    watch: {
+      $route: {
+        handler:function(val, oldVal){
+          // sidebar激活样式方法二
+          console.log("---->页面跳转：", val, oldVal);
+          let _this = this;
 
-    /**
-     * 菜单激活样式，id是当前点击的菜单的id
-     * @param id
-     */
-    activeSidebar: function (id) {
-      // 兄弟菜单去掉active样式，自身增加active样式
-      $("#" + id).siblings().removeClass("active");
-      $("#" + id).siblings().find("li").removeClass("active");
-      $("#" + id).addClass("active");
+          if (!_this.hasResourceRouter(val.name)) {
+            _this.$router.push("/login");
+            return;
+          }
 
-      // 如果有父菜单，父菜单的兄弟菜单去掉open active，父菜单增加open active
-      let parentLi = $("#" + id).parents("li");
-      if (parentLi) {
-        parentLi.siblings().removeClass("open active");
-        parentLi.siblings().find("li").removeClass("active");
-        parentLi.addClass("open active");
+          _this.$nextTick(function(){  //页面加载完成后执行
+            _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+          })
+        }
       }
     },
+    methods: {
+      /**
+       * 查找是否有权限
+       * @param router
+       */
+      hasResourceRouter(router) {
+        let _this = this;
+        let resources = Tool.getLoginUser().resources;
+        if (Tool.isEmpty(resources)) {
+          return false;
+        }
+        for (let i = 0; i < resources.length; i++) {
+          if (router === resources[i].page) {
+            return true;
+          }
+        }
+        return false;
+      },
+      /**
+       * 查找是否有权限
+       * @param id
+       */
+      hasResource(id) {
+        return Tool.hasResource(id);
+      },
+
+      login () {
+        this.$router.push("/admin")
+      },
+
+      /**
+       * 菜单激活样式，id是当前点击的菜单的id
+       * @param id
+       */
+      activeSidebar: function (id) {
+        // 兄弟菜单去掉active样式，自身增加active样式
+        $("#" + id).siblings().removeClass("active");
+        $("#" + id).siblings().find("li").removeClass("active");
+        $("#" + id).addClass("active");
+
+        // 如果有父菜单，父菜单的兄弟菜单去掉open active，父菜单增加open active
+        let parentLi = $("#" + id).parents("li");
+        if (parentLi) {
+          parentLi.siblings().removeClass("open active");
+          parentLi.siblings().find("li").removeClass("active");
+          parentLi.addClass("open active");
+        }
+      },
+
+      logout () {
+        let _this = this;
+        Loading.show();
+        _this.$ajax.get(process.env.VUE_APP_SERVER + '/system/admin/user/logout/' + _this.loginUser.token).then((response)=>{
+          Loading.hide();
+          let resp = response.data;
+          if (resp.success) {
+            Tool.setLoginUser(null);
+            _this.$router.push("/login")
+          } else {
+            Toast.warning(resp.message)
+          }
+        });
+      },
+    }
   }
-
-}
 </script>
-
-
--- admin/src/views/admin/user.vue
+++ admin/src/views/admin/user.vue
@@ -1,79 +1,62 @@
 <template>
-
   <div>
-
     <p>
-
-      <!-- Button trigger modal -->
-      <button @click="add()" class="btn btn-white btn-default btn-round eye-protector-processed"
-              style="transition: background-color 0.3s ease 0s; border-color: rgba(0, 0, 0, 0.35); color: rgb(0, 0, 0); background-color: rgb(193, 230, 198);">
-        <i class="ace-icon fa fa-edit "></i>
+      <button v-show="hasResource('010101')" v-on:click="add()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-edit"></i>
         新增
       </button>
       &nbsp;
-      <button @click="list(1)" class="btn btn-white btn-default btn-round eye-protector-processed"
-              style="transition: background-color 0.3s ease 0s; border-color: rgba(0, 0, 0, 0.35); color: rgb(0, 0, 0); background-color: rgb(193, 230, 198);">
-        <i class="ace-icon fa fa-refresh "></i>
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
         刷新
       </button>
     </p>
 
-    <Pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></Pagination>
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
       <tr>
         <th>id</th>
-
         <th>登陆名</th>
-
         <th>昵称</th>
-
         <th>密码</th>
         <th>操作</th>
       </tr>
       </thead>
 
       <tbody>
-
       <tr v-for="user in users">
-        <td>{{ user.id }}</td>
-        <td>{{ user.loginName }}</td>
-        <td>{{ user.name }}</td>
-        <td>{{ user.password }}</td>
-
+        <td>{{user.id}}</td>
+        <td>{{user.loginName}}</td>
+        <td>{{user.name}}</td>
+        <td>{{user.password}}</td>
         <td>
           <div class="hidden-sm hidden-xs btn-group">
-            <button  v-on:click="editPassword(user)" class="btn btn-xs btn-info">
+            <button v-show="hasResource('010103')" v-on:click="editPassword(user)" class="btn btn-xs btn-info">
               <i class="ace-icon fa fa-key bigger-120"></i>
             </button>
-
-            <button @click="edit(user)" class="btn btn-xs btn-info">
+            <button v-show="hasResource('010101')" v-on:click="edit(user)" class="btn btn-xs btn-info">
               <i class="ace-icon fa fa-pencil bigger-120"></i>
             </button>
-
-            <button @click="del(user.id)" class="btn btn-xs btn-danger">
+            <button v-show="hasResource('010102')" v-on:click="del(user.id)" class="btn btn-xs btn-danger">
               <i class="ace-icon fa fa-trash-o bigger-120"></i>
             </button>
-
           </div>
-
         </td>
       </tr>
-
       </tbody>
     </table>
-    <!-- Modal -->
-    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-              aria-hidden="true">&times;</span></button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">表单</h4>
           </div>
           <div class="modal-body">
             <form class="form-horizontal">
-
               <div class="form-group">
                 <label class="col-sm-2 control-label">登陆名</label>
                 <div class="col-sm-10">
@@ -86,7 +69,7 @@
                   <input v-model="user.name" class="form-control">
                 </div>
               </div>
-              <div class="form-group" v-show="!user.id">
+              <div v-show="!user.id" class="form-group">
                 <label class="col-sm-2 control-label">密码</label>
                 <div class="col-sm-10">
                   <input v-model="user.password" type="password" class="form-control">
@@ -95,25 +78,24 @@
             </form>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
-            <button @click="save" type="button" class="btn btn-primary">保存</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
           </div>
-        </div>
-      </div>
-    </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 
     <div id="edit-password-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-              aria-hidden="true">&times;</span></button>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             <h4 class="modal-title">修改密码</h4>
           </div>
           <div class="modal-body">
             <form class="form-horizontal">
               <div class="form-group">
-                <label class="control-label col-sm-2">新密码</label>
+                <label class="control-label col-sm-2">密码</label>
                 <div class="col-sm-10">
                   <input class="form-control" type="password" v-model="user.password" name="password">
                 </div>
@@ -133,36 +115,40 @@
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-
   </div>
-
 </template>
 
 <script>
-
-  import Pagination from "../../components/pagination"
-
+  import Pagination from "../../components/pagination";
   export default {
-    name: 'system-user',
     components: {Pagination},
-    data: function () {
+    name: "system-user",
+    data: function() {
       return {
         user: {},
         users: [],
-        loginUser: {},
       }
     },
-
-    mounted: function () {
+    mounted: function() {
+      let _this = this;
+      _this.$refs.pagination.size = 5;
+      _this.list(1);
       // sidebar激活样式方法一
       // this.$parent.activeSidebar("system-user-sidebar");
-      let _this = this;
-      _this.$refs.pagination.size = 10;
-      _this.list(1);
 
     },
-
     methods: {
+      /**
+       * 查找是否有权限
+       * @param id
+       */
+      hasResource(id) {
+        return Tool.hasResource(id);
+      },
+
+      /**
+       * 点击【新增】
+       */
       add() {
         let _this = this;
         _this.user = {};
@@ -170,50 +156,36 @@
       },
 
       /**
-       * 修改用户信息
-       *
+       * 点击【编辑】
        */
-       edit(user) {
+      edit(user) {
         let _this = this;
         _this.user = $.extend({}, user);
         $("#form-modal").modal("show");
       },
 
-       editPassword(user) {
-        let _this = this;
-        _this.user = $.extend({}, user);
-        _this.user.password = null;
-        $("#edit-password-modal").modal("show");
-      },
-
-       /**
-       * 点击【查询】
-       * @param page
+      /**
+       * 列表查询
        */
-
       list(page) {
-
         let _this = this;
         Loading.show();
-
         _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/list', {
           page: page,
           size: _this.$refs.pagination.size,
-        }).then((response) => {
+        }).then((response)=>{
           Loading.hide();
-
           let resp = response.data;
           _this.users = resp.content.list;
           _this.$refs.pagination.render(page, resp.content.total);
-        });
+
+        })
       },
 
       /**
        * 点击【保存】
-       * id有值时更新，无值时插入
-       *
        */
-      save: function () {
+      save() {
         let _this = this;
 
         // 保存校验
@@ -228,84 +200,67 @@
 
         _this.user.password = hex_md5(_this.user.password + KEY);
         Loading.show();
-
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/save',
-          _this.user
-        ).then((response) => {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/save', _this.user).then((response)=>{
           Loading.hide();
-
           let resp = response.data;
           if (resp.success) {
             $("#form-modal").modal("hide");
             _this.list(1);
-            Toast.success("保存成功");
-            //修改主界面显示
-            let loginUser = Tool.getLoginUser();
-            if (loginUser.loginName == _this.user.loginName) {
-              loginUser.name = _this.user.name;
-              Tool.setLoginUser(loginUser);
-
-            }
+            Toast.success("保存成功！");
           } else {
-            Toast.warning(resp.message);
+            Toast.warning(resp.message)
           }
-
         })
       },
 
       /**
-       * 点击【保存】
-       * 修改密码
-       * id有值时更新，无值时插入
-       *
+       * 点击【删除】
+       */
+      del(id) {
+        let _this = this;
+        Confirm.show("删除用户表后不可恢复，确认删除？", function () {
+          Loading.show();
+          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/system/admin/user/delete/' + id).then((response)=>{
+            Loading.hide();
+            let resp = response.data;
+            if (resp.success) {
+              _this.list(1);
+              Toast.success("删除成功！");
+            }
+          })
+        });
+      },
+
+      /**
+       * 点击【重置密码】
+       */
+      editPassword(user) {
+        let _this = this;
+        _this.user = $.extend({}, user);
+        _this.user.password = null;
+        $("#edit-password-modal").modal("show");
+      },
+
+      /**
+       * 点击【保存密码】
        */
       savePassword() {
         let _this = this;
 
         _this.user.password = hex_md5(_this.user.password + KEY);
         Loading.show();
-
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/save-password',
-          _this.user
-        ).then((response) => {
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/save-password', _this.user).then((response)=>{
           Loading.hide();
-
           let resp = response.data;
           if (resp.success) {
             $("#edit-password-modal").modal("hide");
             _this.list(1);
-            Toast.success("保存成功");
+            Toast.success("保存成功！");
           } else {
-            Toast.warning(resp.message);
+            Toast.warning(resp.message)
           }
-
         })
       },
-
-      /**
-       * 点击【删除】
-       *
-       */
-      del(id) {
-        let _this = this;
-
-        Confirm.show("删除用户后不可恢复，确认删除？", function () {
-
-            Loading.show();
-            _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/delete/' + id,
-              _this.user
-            ).then((response) => {
-              Loading.hide();
-              let resp = response.data;
-              if (resp.success) {
-                _this.list(1);
-                Toast.success("删除成功！");
-              }
-            })
-          }
-        );
-      }
     }
-
   }
 </script>
