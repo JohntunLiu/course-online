53d0065c6b2b9a4e1245c15b50ff1484b402d8f3
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-16 17:10:58 +0800

    1. 修复资源界面 资源树的显示问题2

--- server/src/main/java/com/course/server/service/ResourceService.java
+++ server/src/main/java/com/course/server/service/ResourceService.java
@@ -19,11 +19,10 @@ import org.springframework.util.StringUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Service
 public class ResourceService {
-    private static final Logger LOG = LoggerFactory.getLogger(ResourceService.class);
 
+    private static final Logger LOG = LoggerFactory.getLogger(ResourceService.class);
 
     @javax.annotation.Resource
     private ResourceMapper resourceMapper;
@@ -34,29 +33,27 @@ public class ResourceService {
     public void list(PageDto pageDto) {
         PageHelper.startPage(pageDto.getPage(), pageDto.getSize());
         ResourceExample resourceExample = new ResourceExample();
-
-
-        List<Resource> resources = resourceMapper.selectByExample(resourceExample);
-        PageInfo<Resource> pageInfo = new PageInfo<>(resources);
+        List<Resource> resourceList = resourceMapper.selectByExample(resourceExample);
+        PageInfo<Resource> pageInfo = new PageInfo<>(resourceList);
         pageDto.setTotal(pageInfo.getTotal());
-        List<ResourceDto> resourceDtoList = CopyUtil.copyList(resources, ResourceDto.class);
+        List<ResourceDto> resourceDtoList = CopyUtil.copyList(resourceList, ResourceDto.class);
         pageDto.setList(resourceDtoList);
-
     }
 
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
     public void save(ResourceDto resourceDto) {
-
         Resource resource = CopyUtil.copy(resourceDto, Resource.class);
         if (StringUtils.isEmpty(resourceDto.getId())) {
             this.insert(resource);
         } else {
             this.update(resource);
         }
-
     }
 
     /**
-     * 新增
+     * 新增，ID是自定义好的，不是自动生成的
      */
     private void insert(Resource resource) {
         resourceMapper.insert(resource);
@@ -76,9 +73,10 @@ public class ResourceService {
         resourceMapper.deleteByPrimaryKey(id);
     }
 
+
+
     /**
      * 保存资源树
-     *
      * @param json
      */
     @Transactional
@@ -86,7 +84,7 @@ public class ResourceService {
         List<ResourceDto> jsonList = JSON.parseArray(json, ResourceDto.class);
         List<ResourceDto> list = new ArrayList<>();
         if (!CollectionUtils.isEmpty(jsonList)) {
-            for (ResourceDto d : jsonList) {
+            for (ResourceDto d: jsonList) {
                 d.setParent("");
                 add(list, d);
             }
@@ -101,14 +99,13 @@ public class ResourceService {
 
     /**
      * 递归，将树型结构的节点全部取出来，放到list
-     *
      * @param list
      * @param dto
      */
     public void add(List<ResourceDto> list, ResourceDto dto) {
         list.add(dto);
         if (!CollectionUtils.isEmpty(dto.getChildren())) {
-            for (ResourceDto d : dto.getChildren()) {
+            for (ResourceDto d: dto.getChildren()) {
                 d.setParent(dto.getId());
                 add(list, d);
             }
@@ -118,7 +115,6 @@ public class ResourceService {
     /**
      * 按约定将列表转成树
      * 要求：ID要正序排列
-     *
      * @return
      */
     public List<ResourceDto> loadTree() {
@@ -131,7 +127,7 @@ public class ResourceService {
             ResourceDto child = resourceDtoList.get(i);
 
             // 如果当前节点没有父节点，则不用往下了
-            if (StringUtils.hasLength(child.getParent())) {
+            if (StringUtils.isEmpty(child.getParent())) {
                 continue;
             }
             // 查找父节点
@@ -143,6 +139,7 @@ public class ResourceService {
                     }
                     // 添加到最前面，否则会变成倒序，因为循环是从后往前循环的
                     parent.getChildren().add(0, child);
+
                     // 子节点找到父节点后，删除列表中的子节点
                     resourceDtoList.remove(child);
                 }
@@ -150,6 +147,4 @@ public class ResourceService {
         }
         return resourceDtoList;
     }
-
-
 }
