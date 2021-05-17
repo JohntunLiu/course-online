57ff41530d6394f2744771bc801c9791d3c67175
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-17 20:06:28 +0800

    1. 网站开发: 列表list.vue, 配置路由, 后端接口只能查询已发布数据

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -2,9 +2,9 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">
+        <router-link to="/index" class="navbar-brand" href="#">
           <i class="ace-icon fa fa-video-camera"></i>&nbsp;蝉语课程
-        </a>
+        </router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
--- web/src/router.js
+++ web/src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Index from "./views/index.vue"
-// import List from "./views/list.vue"
+import List from "./views/list.vue"
 // import Detail from "./views/detail.vue"
 
 Vue.use(Router);
@@ -16,10 +16,11 @@ export default new Router({
         path: "/index",
         component: Index
     }
-        //, {
-    //     path: "/list",
-    //     component: List
-    // }, {
+        , {
+        path: "/list",
+        component: List
+    },
+        // {
     //     path: "/detail",
     //     component: Detail
     // }
