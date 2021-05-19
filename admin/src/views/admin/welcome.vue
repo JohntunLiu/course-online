495541f350b5c3ff3652dd84b6f4cbf05a0fc080
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-19 22:51:20 +0800

    1. 控制台欢迎界面: 增加顶部统计

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -1,27 +1,78 @@
 <template>
   <div>
-      <h1>hello tang</h1>
+    <div class="space-20"></div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="center">
+          <span class="btn btn-app btn-sm btn-light no-hover">
+            <span class="line-height-1 bigger-170 blue"> 518 </span>
+
+            <br/>
+            <span class="line-height-1 smaller-90"> 课程 </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-yellow no-hover">
+													<span class="line-height-1 bigger-170"> 32 </span>
+
+													<br/>
+													<span class="line-height-1 smaller-90"> 大章 </span>
+												</span>
+
+          <span class="btn btn-app btn-sm btn-pink no-hover">
+													<span class="line-height-1 bigger-170"> 4 </span>
+
+													<br/>
+													<span class="line-height-1 smaller-90"> 小节 </span>
+												</span>
+
+          <span class="btn btn-app btn-sm btn-grey no-hover">
+													<span class="line-height-1 bigger-170"> 23 </span>
+
+													<br/>
+													<span class="line-height-1 smaller-90"> 会员(万) </span>
+												</span>
+
+          <span class="btn btn-app btn-sm btn-success no-hover">
+													<span class="line-height-1 bigger-170"> 7 </span>
+
+													<br/>
+													<span class="line-height-1 smaller-90"> 评论(万) </span>
+												</span>
+
+          <span class="btn btn-app btn-sm btn-primary no-hover">
+													<span class="line-height-1 bigger-170"> 55 </span>
+
+													<br/>
+													<span class="line-height-1 smaller-90"> Contacts </span>
+												</span>
+        </div>
+
+
+
+
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'welcome',
-  data: function () {
-    return {
-      loginUser: {},
-      imageCodeToken: ""
-    }
-  },
-  mounted: function () {
-    // sidebar激活样式方法一
-    // this.$parent.activeSidebar("welcome-sidebar");
+  export default {
+    name: 'welcome',
+    data: function () {
+      return {
+        loginUser: {},
+        imageCodeToken: ""
+      }
+    },
+    mounted: function () {
+      // sidebar激活样式方法一
+      // this.$parent.activeSidebar("welcome-sidebar");
 
-  },
+    },
 
-  methods: {
+    methods: {}
 
   }
-
-}
 </script>
