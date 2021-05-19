04444da4dda1cf5a839ae154c4ecc7ca9d7c3c02
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-19 23:06:34 +0800

    1. 控制台欢迎界面: 增加新增评论, 注册会员, 课程报名统计显示

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -3,7 +3,7 @@
     <div class="space-20"></div>
     <div class="row">
       <div class="col-xs-12">
-        <div class="center">
+        <div class="text-center">
           <span class="btn btn-app btn-sm btn-light no-hover">
             <span class="line-height-1 bigger-170 blue"> 518 </span>
 
@@ -36,22 +36,70 @@
 													<span class="line-height-1 bigger-170"> 7 </span>
 
 													<br/>
-													<span class="line-height-1 smaller-90"> 评论(万) </span>
+													<span class="line-height-1 smaller-90"> 报名(万) </span>
 												</span>
 
           <span class="btn btn-app btn-sm btn-primary no-hover">
 													<span class="line-height-1 bigger-170"> 55 </span>
 
 													<br/>
-													<span class="line-height-1 smaller-90"> Contacts </span>
+													<span class="line-height-1 smaller-90"> 评论(万) </span>
 												</span>
         </div>
+        <div class="space-12"></div>
 
 
+        <div class="hr hr2 hr-double"></div>
 
+        <div class="space-12"></div>
 
 
       </div>
+
+      <div class="col-sm-12 infobox-container ">
+        <div class="infobox infobox-green">
+          <div class="infobox-icon">
+            <i class="ace-icon fa fa-comments"></i>
+          </div>
+
+          <div class="infobox-data">
+            <span class="infobox-data-number">32</span>
+            <div class="infobox-content">新增评论</div>
+          </div>
+
+          <div class="stat stat-success">8%</div>
+        </div>
+
+        <div class="infobox infobox-blue">
+          <div class="infobox-icon">
+            <i class="ace-icon fa fa-twitter"></i>
+          </div>
+
+          <div class="infobox-data">
+            <span class="infobox-data-number">11</span>
+            <div class="infobox-content">新注册会员</div>
+          </div>
+
+          <div class="badge badge-success">
+            +32%
+            <i class="ace-icon fa fa-arrow-up"></i>
+          </div>
+        </div>
+
+        <div class="infobox infobox-pink">
+          <div class="infobox-icon">
+            <i class="ace-icon fa fa-shopping-cart"></i>
+          </div>
+
+          <div class="infobox-data">
+            <span class="infobox-data-number">8</span>
+            <div class="infobox-content">课程报名</div>
+          </div>
+          <div class="stat stat-important">4%</div>
+        </div>
+
+      </div>
+
     </div>
 
   </div>
