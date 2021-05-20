4d5402446ab1de6e00cd8eea3cb302d392ba1731
羽蝉 <46100812+liuzongtang@users.noreply.github.com>
2021-05-20 10:43:56 +0800

    1. 控制台欢迎界面: 增加课程销售排名和分类销售排名统计显示

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -128,6 +128,130 @@
         <div class="space-12"></div>
       </div><!-- /.col -->
 
+      <div class="col-sm-6">
+        <div class="widget-box transparent">
+          <div class="widget-header widget-header-flat">
+            <h4 class="widget-title lighter">
+              <i class="ace-icon fa fa-star orange"></i>
+              课程销售排名
+            </h4>
+          </div>
+
+          <div class="widget-body">
+            <div class="widget-main no-padding">
+              <table class="table table-bordered table-striped">
+                <thead class="thin-border-bottom">
+                <tr>
+                  <th>
+                    <i class="ace-icon fa fa-caret-right blue"></i>课程名称
+                  </th>
+
+                  <th>
+                    <i class="ace-icon fa fa-caret-right blue"></i>价格
+                  </th>
+
+                  <th class="hidden-480">
+                    <i class="ace-icon fa fa-caret-right blue"></i>购买数
+                  </th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                  <td>internet.com</td>
+
+                  <td>
+                    <small>
+                      <s class="red">$29.99</s>
+                    </small>
+                    <b class="green">$19.99</b>
+                  </td>
+
+                  <td class="hidden-480">
+                    <span class="label label-success arrowed arrowed-right">6745</span>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>online.com</td>
+
+                  <td>
+                    <b class="blue">$16.45</b>
+                  </td>
+
+                  <td class="hidden-480">
+                    <span class="label label-success arrowed arrowed-right">6745</span>
+
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>newnet.com</td>
+
+                  <td>
+                    <b class="blue">$15.00</b>
+                  </td>
+
+                  <td class="hidden-480">
+                    <span class="label label-warning arrowed arrowed-right">6745</span>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>web.com</td>
+
+                  <td>
+                    <small>
+                      <s class="red">$24.99</s>
+                    </small>
+                    <b class="green">$19.95</b>
+                  </td>
+
+                  <td class="hidden-480">
+                    <span class="label label-warning arrowed arrowed-right">6745</span>
+
+                  </td>
+                </tr>
+
+                <tr>
+                  <td>domain.com</td>
+
+                  <td>
+                    <b class="blue">$12.00</b>
+                  </td>
+
+                  <td class="hidden-480">
+                    <span class="label label-warning arrowed arrowed-right">6745</span>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div><!-- /.widget-main -->
+          </div><!-- /.widget-body -->
+        </div><!-- /.widget-box -->
+      </div><!-- /.col -->
+
+
+      <div class="col-sm-6">
+        <div class="widget-box transparent">
+          <div class="widget-header widget-header-flat">
+            <h4 class="widget-title lighter">
+              <i class="ace-icon fa fa-star orange"></i>
+              课程销售占比
+            </h4>
+          </div>
+
+          <div class="widget-body">
+            <div class="widget-main">
+              <div id="piechart-placeholder"></div>
+
+
+            </div><!-- /.widget-main -->
+          </div><!-- /.widget-body -->
+        </div><!-- /.widget-box -->
+      </div><!-- /.col -->
+
+
     </div><!-- /.row -->
 
   </div>
@@ -147,6 +271,7 @@
       // this.$parent.activeSidebar("welcome-sidebar");
       let _this = this;
       _this.drawSaleChart();
+      _this.drawPieChart();
 
     },
 
@@ -162,17 +287,17 @@
           d2.push([i + 1, 1900 + Math.floor((Math.random() * 100) + 1)]);
         }
 
-        let sales_charts = $('#sales-charts').css({'width':'100%' , 'height':'220px'});
+        let sales_charts = $('#sales-charts').css({'width': '100%', 'height': '220px'});
 
         $.plot("#sales-charts", [
-          { label: "最近30天", data: d1 },
-          { label: "上一周期", data: d2 },
+          {label: "最近30天", data: d1},
+          {label: "上一周期", data: d2},
         ], {
           hoverable: true,
           shadowSize: 0,
           series: {
-            lines: { show: true },
-            points: { show: true }
+            lines: {show: true},
+            points: {show: true}
           },
           xaxis: {
             tickLength: 0
@@ -182,14 +307,50 @@
 
           },
           grid: {
-            backgroundColor: { colors: [ "#fff", "#fff" ] },
+            backgroundColor: {colors: ["#fff", "#fff"]},
             borderWidth: 1,
-            borderColor:'#555'
+            borderColor: '#555'
           }
         });
       },
+      drawPieChart() {
+        let placeholder = $('#piechart-placeholder').css({'width': '90%', 'min-height': '190px'});
+        let data = [
+          {label: "Java", data: 38.7, color: "#68BC31"},
+          {label: "Python", data: 24.5, color: "#2091CF"},
+          {label: "Android", data: 8.2, color: "#AF4E96"},
+          {label: "其他", data: 28.6, color: "#DA5430"},
+        ];
+        $.plot(placeholder, data, {
+          series: {
+            pie: {
+              show: true,
+              tilt: 0.8,
+              highlight: {
+                opacity: 0.25
+              },
+              stroke: {
+                color: '#fff',
+                width: 2
+              },
+              startAngle: 2
+            }
+          },
+          legend: {
+            show: true,
+            labelBoxBorderColor: null,
+            margin: [-30, 15]
+          }
+          ,
+          grid: {
+            hoverable: true,
+            clickable: true
+          }
+        })
+      },
 
-    }
+    },
+
 
   }
 </script>
